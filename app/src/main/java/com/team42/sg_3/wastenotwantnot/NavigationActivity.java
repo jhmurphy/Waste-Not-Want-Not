3a0df97f609f104a1217c4824213e6175a7a3efa
Carter Scheve <cascheve@iastate.edu>
2017-02-05 14:10:54 -0600

    added settings base page

--- app/src/main/java/com/team42/sg_3/wastenotwantnot/NavigationActivity.java
+++ app/src/main/java/com/team42/sg_3/wastenotwantnot/NavigationActivity.java
@@ -1,5 +1,6 @@
 package com.team42.sg_3.wastenotwantnot;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class NavigationActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -68,9 +70,10 @@ public class NavigationActivity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
