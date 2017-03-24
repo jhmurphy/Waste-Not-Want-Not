a5ce1bea513717ae6ea3802ccb0f240ef169a4a2
Carter Scheve <cascheve@iastate.edu>
2017-03-24 11:26:35 -0500

    added push notification functionality to creation of posts

--- DiscussionBoard.php
+++ DiscussionBoard.php
@@ -7,8 +7,6 @@ $db_server = "mysql.cs.iastate.edu";
 $db_name = "db309sg3";
 //echo $db_username . " : " . $db_password . "</br>";
 $conn = new mysqli($db_server, $db_username, $db_password, $db_name);
-$path_to_fcm = "https://fcm.googleapis.com/fcm/send";
-$server_key = "AAAAhRKOCo0:APA91bGofPDpw-RTs91YLQT1kR_C9cto0yZTLwEHHYtaGXf9sXicTtrt5EEeHD_cG07ZOBABGYVd2yc2YqTK1ojTbe6b9_jwAbP9VJLsD25ObkWFXknBBa8fmYf7dpBMGfdo3Ei5Bb6N";
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
@@ -22,39 +20,6 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
 	while ($row = mysqli_fetch_assoc($result)) {
 		$data[] = $row['ThreadName'];
 	}
- 
-	
-	$query = "SELECT fcm_token FROM Users WHERE username = 'cscheve'";
-	$result = $conn->query($query) or die("error making request");
-	$row = mysqli_fetch_row($result);
-	$token = $row[0];
-	
-	$headers = array(
-		'Authorization:key=' . $server_key,
-		'Content-Type:application/json'
-	);
-	
-	$title = "Thread Update";
-	$message = $username . " has made a post in your thread";
-	
-	$fields = array(
-		'to'=>$token,
-		'data'=>array('title'=>$title,'body'=>$message)
-	);
-	
-	$payload = json_encode($fields);
-	
-	$curl_session = curl_init();
-	curl_setopt($curl_session, CURLOPT_URL, $path_to_fcm);
-	curl_setopt($curl_session, CURLOPT_POST, true);
-	curl_setopt($curl_session, CURLOPT_HTTPHEADER, $headers);
-	curl_setopt($curl_session, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($curl_session, CURLOPT_SSL_VERIFYPEER, false);
-	curl_setopt($curl_session, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
-	curl_setopt($curl_session, CURLOPT_POSTFIELDS, $payload);
-	
-	$result = curl_exec($curl_session);
-	curl_close($curl_session);
 
 	if($result){
 		echo json_encode($data);
