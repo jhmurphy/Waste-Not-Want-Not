d6ed06729416fd285fbf4be55236b6c561982b2b
Cory Johannes <johannes@iastate.edu>
2017-04-26 22:16:58 -0500

    Added functionality to addEvent and internalStorage

--- app/src/main/java/com/team42/sg_3/wastenotwantnot/internalStorage.java
+++ app/src/main/java/com/team42/sg_3/wastenotwantnot/internalStorage.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -17,58 +18,121 @@ import static android.content.Context.MODE_PRIVATE;
 
 public class internalStorage {
 
-    public internalStorage(){}
+    private Context context;
 
-    public void inputEvent(String title, long start, long end){
+    public internalStorage(Context con){
+        context = con;
+    }
 
-        SQLiteDatabase db = SQLiteDatabase.openOrCreateDatabase("309WNWN", null);
-        db.execSQL("CREATE TABLE IF NOT EXISTS Events(Title STRING, Start text, End text);");
+
+    /**
+     * Puts event into sqlite table events
+     * @param title
+     * @param start
+     * @param end
+     * @return
+     */
+    public String inputEvent(String title, long start, long end){
+
+        SQLiteDatabase db = context.openOrCreateDatabase("309WNWN", Context.MODE_PRIVATE, null);
+
+        db.execSQL("CREATE TABLE IF NOT EXISTS Events(Title STRING, Start long, End long);");//Maybe change long to text
         Date sd = new Date(start);
         Date ed = new Date(end);
 
-        String startDate = ""+sd.getYear()+"-"+sd.getMonth()+"-"+sd.getDay()+" "+sd.getHours()%24+":"+sd.getMinutes()%60+":"+sd.getSeconds()%60+".000";
-        String endDate = ""+ed.getYear()+"-"+ed.getMonth()+"-"+ed.getDay()+" "+ed.getHours()%24+":"+ed.getMinutes()%60+":"+ed.getSeconds()%60+".000";
+        //String startDate = ""+sd.getYear()+"-"+sd.getMonth()+"-"+sd.getDay()+" "+sd.getHours()%24+":"+sd.getMinutes()%60+":"+sd.getSeconds()%60+".000";
+        //String endDate = ""+ed.getYear()+"-"+ed.getMonth()+"-"+ed.getDay()+" "+ed.getHours()%24+":"+ed.getMinutes()%60+":"+ed.getSeconds()%60+".000";
 
-        db.execSQL("INSERT INTO Events Values "+ title + ", "+startDate+", " + endDate + ";");
+        db.execSQL("INSERT INTO Events(Title, Start, End) VALUES('"+ title + "', '"+start+"', '" + end + "');");
 
-        return;
+        //db.execSQL("INSERT INTO Events(Title, Start, End) VALUES('"+ title + "', '"+startDate+"', '" + endDate + "');");
+
+        String s = "Event "+title + " Added for "+ millisToDate(start) + " to " + millisToDate(end);
+
+        return s;
 
     }
 
-    public ArrayList<String[]> retrieveEvents(){
-        ArrayList<String[]> array = new ArrayList<String[]>();
+    /**
+     * returns sorted arraylist of object arrays. for each array, 1st item is string, 2nd and 3rd are longs
+     * @return
+     */
+    public ArrayList<Object[]> retrieveEvents(){
+        ArrayList<Object[]> array = new ArrayList<Object[]>();
         SQLiteDatabase db = SQLiteDatabase.openOrCreateDatabase("309WNWN", null);
         Cursor c = db.rawQuery("SELECT * FROM Events", null);
         c.moveToFirst();
         int i = 0;
         while(!c.isAfterLast()){
-            String[] s = new String[3];
+            Object[] s = new Object[3];
             s[0] = c.getString(c.getColumnIndex("Title"));
-            s[1] = c.getString(c.getColumnIndex("Start"));
-            s[2] = c.getString(c.getColumnIndex("End"));
+            s[1] = c.getLong(c.getColumnIndex("Start"));
+            s[2] = c.getLong(c.getColumnIndex("End"));
             array.add(i, s);
             i++;
             c.moveToNext();
         }
         c.close();
+        sort(array);
         return array;
     }
 
+    private ArrayList<Object[]> sort(ArrayList<Object[]> toSort){
+        ArrayList<Object[]> sorted = toSort;
+        Object[] temp;
+        int i, j;
+        for(i = 0; i < sorted.size(); i++){
+            for(j = i + 1; j < sorted.size(); j++){
+                if((long)sorted.get(i)[1] > (long)sorted.get(j)[1]){
+                    temp = sorted.get(i);
+                    sorted.get(i)[0] = sorted.get(j)[0];
+                    sorted.get(i)[1] = sorted.get(j)[1];
+                    sorted.get(i)[2] = sorted.get(j)[2];
+                    sorted.get(j)[0] = temp[0];
+                    sorted.get(j)[1] = temp[1];
+                    sorted.get(j)[2] = temp[2];
+                }
+            }
+        }
+        return sorted;
+    }
+
+    /**
+     * returns string of date represented by long
+     * @param m
+     * @return
+     */
+    public String millisToDate(long m){
+        Date date = new Date(m);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy MM dd HH:mm:ss");
+        String text = sdf.format(date);
+        return text;
+    }
+
+    /**
+     * deletes specefied event
+     * @param title
+     * @param start
+     * @param end
+     */
     public void deleteEvent(String title, long start, long end){
         SQLiteDatabase db = SQLiteDatabase.openOrCreateDatabase("309WNWN", null);
-
+        /*
         Date sd = new Date(start);
         Date ed = new Date(end);
 
         String startDate = ""+sd.getYear()+"-"+sd.getMonth()+"-"+sd.getDay()+" "+sd.getHours()%24+":"+sd.getMinutes()%60+":"+sd.getSeconds()%60+".000";
         String endDate = ""+ed.getYear()+"-"+ed.getMonth()+"-"+ed.getDay()+" "+ed.getHours()%24+":"+ed.getMinutes()%60+":"+ed.getSeconds()%60+".000";
-
-        db.execSQL("DELETE * FROM Events WHERE Title = " + title + " AND Start = " + startDate + " AND End = " + endDate);
+        */
+        db.execSQL("DELETE * FROM Events WHERE Title = " + title + " AND Start = " + start + " AND End = " + end);
     }
 
+    /**
+     * deletes all events that have ended
+     */
     public void cleanEvents(){
         SQLiteDatabase db = SQLiteDatabase.openOrCreateDatabase("309WNWN", null);
-        db.execSQL("DELETE * FROM Events WHERE julianday('now') > julianday(End)");
+        db.execSQL("DELETE * FROM Events WHERE End < " + System.currentTimeMillis() + ";");
         return;
     }
 }
