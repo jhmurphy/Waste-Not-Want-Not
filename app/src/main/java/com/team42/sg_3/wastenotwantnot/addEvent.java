036ceab523722a2b8bbbf1f19aecb8c4a0c5ca21
Cory Johannes <johannes@iastate.edu>
2017-04-27 17:52:00 -0500

    Fixed issue with date

--- app/src/main/java/com/team42/sg_3/wastenotwantnot/addEvent.java
+++ app/src/main/java/com/team42/sg_3/wastenotwantnot/addEvent.java
@@ -1,8 +1,6 @@
 package com.team42.sg_3.wastenotwantnot;
 
 import android.annotation.TargetApi;
-import android.content.Context;
-import android.provider.CalendarContract;
 import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -57,7 +55,7 @@ public class addEvent extends AppCompatActivity{
         int d = day.getDayOfMonth();
         long Smillis = 0;
         long Emillis = 0;
-
+        String StartToMillis = "";
         Intent in = new Intent(Intent.ACTION_EDIT);
         in.setType("vnd.android.cursor.item/event");
 
@@ -67,9 +65,9 @@ public class addEvent extends AppCompatActivity{
             return;
         }else{
             //Turning start and end times into longs
-            String StartToMillis = yr+"/"+mon+"/"+d+" "+startHour+":"+startMin+":00";
-            String EndToMillis = yr+"/"+mon+"/"+d+" "+endHour+":"+endMin+":00";
-            SimpleDateFormat s = new SimpleDateFormat("yyyy/mm/dd hh:mm:ss");
+            StartToMillis = yr+" "+mon+" "+d+" "+startHour+":"+startMin+":00";
+            String EndToMillis = yr+" "+mon+" "+d+" "+endHour+":"+endMin+":00";
+            SimpleDateFormat s = new SimpleDateFormat("yyyy MM dd hh:mm:ss");
             Date date1;
             Date date2;
             try {
@@ -86,7 +84,6 @@ public class addEvent extends AppCompatActivity{
 
         internalStorage is = new internalStorage(getApplicationContext());
         String proof = is.inputEvent(ename, Smillis, Emillis);
-
         Toast.makeText(addEvent.this, proof, Toast.LENGTH_LONG).show();
 
         back(view);
